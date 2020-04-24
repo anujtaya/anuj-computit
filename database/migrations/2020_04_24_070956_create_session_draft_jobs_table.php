b366e1efedddec6185ac9ec3832d78d0a2cb211d
anujtaya <tayaanuj@gmail.com>
2020-04-24 13:43:20 +0530

    Updated temporary job data session table migration content

--- database/migrations/2020_04_24_070956_create_session_draft_jobs_table.php
+++ database/migrations/2020_04_24_070956_create_session_draft_jobs_table.php
@@ -14,7 +14,24 @@ class CreateSessionDraftJobsTable extends Migration
     public function up()
     {
         Schema::create('session_draft_jobs', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->string('id')->unique();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            //job information
+            $table->string('title')->nullable();
+            $table->char('status', 12)->nullable(); //DRAFT
+            $table->text('description')->nullable();
+            $table->decimal("job_lat", 9,6)->nullable();
+            $table->decimal("job_lng", 9,6)->nullable();
+            $table->string('street_number')->nullable();
+            $table->string('street_name')->nullable();
+            $table->string('state')->nullable();
+            $table->string('suburb')->nullable();
+            $table->string('city')->nullable();
+            $table->string('postcode')->nullable();
+            $table->string('service_category_name')->nullable();
+            $table->string('service_subcategory_name')->nullable();
+            $table->bigInteger('service_category_id')->unsigned()->nullable();
             $table->timestamps();
         });
     }
