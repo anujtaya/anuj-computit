04e68198fd4174aebce953d1b447772894a0144d
anujtaya <tayaanuj@gmail.com>
2020-04-29 22:00:03 +0530

    Updated LocaL2LoaL logo spinne design on all pages.

--- resources/views/layouts/service_provider_master.blade.php
+++ resources/views/layouts/service_provider_master.blade.php
@@ -41,7 +41,7 @@
          </div> -->
       <div id="anim-1" style="background:rgba(255, 252, 252, 0.68);position:fixed;top:0;left:0;z-index:10000!important;height:100%;width:100%;display:none!important;">
          <div class="text-center" style="margin-top:250px;">
-            <img src="{{asset('images/svg/eclipse_loader_sp.svg')}}?v=12" height="60" width="60" alt="">
+            <img src="{{secure_url('/images/brand/l2l-logo-svg.svg')}}" class="fa-spin spin" height="60" width="60">
          </div>
          <div class="text-center ml-4 mr-4 d-none" style="margin-top:20px;" id="anim-2">
          </div>
