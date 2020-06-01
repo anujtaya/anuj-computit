de5a89719c79206c124b9816a54770a4b6041582
anujtaya <tayaanuj@gmail.com>
2020-06-01 11:06:58 +0530

    Changed the job description text to purple on service seeker conversation page

--- resources/views/service_seeker/jobs/job_conversation_partial.blade.php
+++ resources/views/service_seeker/jobs/job_conversation_partial.blade.php
@@ -11,7 +11,7 @@
     <!-- first message as job description -->
     <div class="media w-50   mb-3">
          <div class="media-body ml-2">
-            <div class=" text-white  py-2 px-3 mb-2 rounded" style="background:#399BDB;">
+            <div class=" text-white  py-2 px-3 mb-2 rounded" style="background:#5D29BA!important;color:white!important;">
                <span class="text-small">Job Description:</span>
                <p class="text-small text-white mb-0 "><i>{{$job->description}}</i></p>
             </div>
