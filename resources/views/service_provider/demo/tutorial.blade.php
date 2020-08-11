217226b29fda1bd9d015c45ee83010a228c99ef6
anujtaya <tayaanuj@gmail.com>
2020-08-11 18:56:14 +0530

    Added custom shadow to service provider tutorial and minor adjustements to service provider tutorial layout.

--- resources/views/service_provider/demo/tutorial.blade.php
+++ resources/views/service_provider/demo/tutorial.blade.php
@@ -2,49 +2,60 @@
 @section('content')
 <style>
 .carousel-indicators li {
-   background-color: #5D29BA;
+   /* background-color: #5D29BA; */
+   background-color:#7131A1;
+   border-radius: 50%;
+   width:15px;
+   height:15px;
+}
+
+.sub-text-color {
+   color:#B4B4B4!important;
+   font-size:12px!important;
+}
+
+h5 {
+   font-weight:bold!important;
 }
 </style>
 <div class="container">
    <div class="row  justify-content-center" >
-      <div class="col-lg-4  ">
+   <div class="col-lg-4  ">
          <div class="row   " >
             <div class="col-md-12 h-100  " >
                <div class="card bg-white p-0 text-center shadow-none">
-                  <div class="mt-4 borders  bg-darks text-center">
-                     <div id="carouselExampleIndicators" class="carousel slide" style="min-height:500px!important;" data-ride="carousel">
+                  <div class=" borders  bg-darks text-center">
+                     <div id="carouselExampleIndicators" class="carousel slide" style="min-height:470px!important;" data-ride="carousel">
                      <ol class="carousel-indicators mt-5">
                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                         </ol>
                         <div class="carousel-inner">
-                           <div class="carousel-item active p-3">
-                              <img src="{{asset('/images/svg/l2l_service_provider.svg')}}" style="width:250;" class="img-fluid m-4" alt="">
-                              <h5 class="fs-1 mt-2">Search for jobs</h5>
-                              <p class="fs--1 mt-2">                      
+                           <div class="carousel-item active p-2">
+                              <img src="{{asset('/images/svg/l2l_service_provider.svg')}}" style="height:200px!important;width:200px!important;" class="img-fluid m-4" alt="">
+                              <h5 class="fs-1 ">Search for jobs</h5>
+                              <p class="fs--1  sub-text-color">                      
                                  You can now search for jobs within your local area. A person looking for work done 
                                  will post their job with a bit of information and any photos necessary. You can than filter this search 
                                  based upon your interests and skills provided when setting up your profile or customise it further to suit you.
                               </p>
                            </div>
-                           <div class="carousel-item  p-3">
-                              <img src="{{asset('/images/svg/l2l_user_laptop.svg')}}" style="width:250;" class="img-fluid m-4" alt="">
-                              <h5 class="fs-1 mt-2">Make an Offer</h5>
-                              <p class="fs--1 mt-2">
+                           <div class="carousel-item  p-2">
+                              <img src="{{asset('/images/svg/l2l_user_laptop.svg')}}" style="height:200px!important;width:200px!important;" class="img-fluid m-4" alt="">
+                              <h5 class="fs-1">Make an Offer</h5>
+                              <p class="fs--1  sub-text-color">
                                  Find a job you like? Its as simple as clicking on the job in our jobs board, describing why you would be great at
                                  the job and making your pricing offer on the job. This will send a message to the seeker who will be able to compare 
                                  you and your pricing, location, reviews, rating against other providers who have offered to do the job. 
                               </p>
                            </div>
-                           <div class="carousel-item p-3">
-                              <img src="{{asset('/images/svg/l2l_user_clock.svg')}}" style="width:250;" class="img-fluid m-4" alt="">
-                              <h5 class="fs-1 mt-2">Set your time and date and you are on your way!</h5>
-                              <p class="fs--1 mt-2">
+                           <div class="carousel-item p-2">
+                              <img src="{{asset('/images/svg/l2l_user_clock.svg')}}" style="height:200px!important;width:200px!important;" class="img-fluid m-4" alt="">
+                              <h5 class="fs-1">Set your time and date and you are on your way!</h5>
+                              <p class="fs--1  sub-text-color">
                                  If you have been selected to do the job, you will get a notificaion which will allow you to use our in app messaging
-                                 features to negotiate the best time and date that suits you. Once agreed upon the rest is up to you. In app map
-                                 view will direct you to your assigned location and you will be able to complete the job, get paid and be on your way to 
-                                 the next one.
+                                 features to negotiate the best time and date that suits you. In app map view will direct you to your assigned location and you will be able to complete the job, get paid.
                               </p>
                            </div>
                         </div>
@@ -58,7 +69,7 @@
                         </a>
                      </div>
                     <br>
-                    <a href="{{route('guest_service_provider_home')}}" onclick="toggle_animation(true);"  class="btn theme-background-color rounded-3 borders card-1 ml-4 mr-4 mt-4 font-weight-normal">Continue <i class="ml-2 fas fa-arrow-right"></i></a>
+                    <a href="{{route('guest_service_provider_home')}}" onclick="toggle_animation(true);"  class="btn theme-background-color rounded-3 borders card-1 ml-4 mr-4 font-weight-normal custom_button_shadow_sp">Continue <i class="ml-2 fas fa-arrow-right"></i></a>
 
                </div>
             </div>
--- resources/views/service_seeker/demo/tutorial.blade.php
+++ resources/views/service_seeker/demo/tutorial.blade.php
@@ -2,7 +2,19 @@
 @section('content')
 <style>
 .carousel-indicators li {
-   background-color: #399BDB;
+   /* background-color: #399BDB; */
+  background-color:#389BDB;
+  border-radius: 50%;
+  width:15px;
+  height:15px;
+}
+
+.sub-text-color {
+   color:#B4B4B4!important;
+}
+
+h5 {
+   font-weight:bold!important;
 }
 </style>
 <div class="container">
@@ -12,42 +24,42 @@
             <div class="col-md-12 h-100  " >
                <div class="card bg-white p-0 text-center shadow-none">
                   <div class="borders  bg-darks text-center">
-                     <div id="carouselExampleIndicators" class="carousel slide" style="min-height:500px!important;" data-ride="carousel">
-                        <ol class="carousel-indicators">
+                     <div id="carouselExampleIndicators" class="carousel slide" style="min-height:470px!important;" data-ride="carousel">
+                        <ol class="carousel-indicators" style="margin-top:50px!important;">
                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
                         </ol>
                         <div class="carousel-inner">
-                           <div class="carousel-item active p-2">
-                              <img src="{{asset('/images/svg/l2l_ss_relaxing_at_home.svg')}}" style="width:250;min-height:260px!important;" class="img-fluid m-4" alt="">
+                           <div class="carousel-item active p-2 text-center">
+                              <img src="{{asset('/images/svg/l2l_ss_relaxing_at_home.svg')}}" style="height:200px!important;width:200px!important;" class="img-fluid m-4" alt="">
                               <h5 class="fs-1 mt-2">Select a Service</h5>
-                              <p class="fs--1 mt-2">                      
+                              <p class="fs--1 mt-2 sub-text-color">                      
                                  Pick from over 200 categories, describe your job, set a time and we'll connect.
                               </p>
                            </div>
-                           <div class="carousel-item  p-2">
-                              <img src="{{asset('/images/svg/l2l_ss_meeting.svg')}}" style="width:250;min-height:260px!important;"  class="img-fluid m-4" alt="">
+                           <div class="carousel-item  p-2 text-center">
+                              <img src="{{asset('/images/svg/l2l_ss_meeting.svg')}}" style="height:200px!important;width:200px!important;"  class="img-fluid m-4" alt="">
                               <h5 class="fs-1 mt-2">Pick a Service Provider</h5>
-                              <p class="fs--1 mt-2">
-                                 Now that your job has been requested, Service Providers will be notified instantly and are able to accpet or decline the job depending on their
+                              <p class="fs--1 mt-2 sub-text-color">
+                                 Now that your job has been requested, Service Providers will be notified instantly and are able to accept or decline the job depending on their
                                  preference.
                               </p>
                            </div>
                            <div class="carousel-item p-2">
-                              <img src="{{asset('/images/svg/l2l_ss_time_management.svg')}}" style="width:250;min-height:260px!important;"  class="img-fluid m-4" alt="">
+                              <img src="{{asset('/images/svg/l2l_ss_time_management.svg')}}" style="height:200px!important;width:200px!important;"  class="img-fluid m-4" alt="">
                               <h5 class="fs-1 mt-2">Set your time and date</h5>
-                              <p class="fs--1 mt-2">
+                              <p class="fs--1 mt-2 sub-text-color">
                                  Once accepted, we hoe to have your job completed ASAP. You can scheule the time for immediately or sometime in the future. Our in app messaging
                                  also allows you to contact the selected provider to negotiate a time that suits you both.
                               </p>
                            </div>
                            <div class="carousel-item p-2">
-                              <img src="{{asset('/images/svg/l2l_ss_location_tracking.svg')}}" style="width:250;min-height:260px!important;"  class="img-fluid m-4" alt="">
+                              <img src="{{asset('/images/svg/l2l_ss_location_tracking.svg')}}" style="height:200px!important;width:200px!important;"  class="img-fluid m-4" alt="">
                               <h5 class="fs-1 mt-2">Live Tracking</h5>
-                              <p class="fs--1 mt-2">
-                                 Our live tracking feature allows you to tune in live and watch your provider exact location adn ETA.
+                              <p class="fs--1 mt-2 sub-text-color">
+                                 Our live geo tracking technology allows you to see your provider arriving at your location in real time.
                               </p>
                            </div>
                         </div>
@@ -61,7 +73,7 @@
                         </a>
                      </div>
                     <br>
-                    <a href="{{route('guest_service_seeker_home')}}" onclick="toggle_animation(true);"  class="btn theme-background-color rounded-3 borders card-1 ml-4 mr-4 mt-4 font-weight-normal">Continue <i class="ml-2 fas fa-arrow-right"></i></a>
+                    <a href="{{route('guest_service_seeker_home')}}" onclick="toggle_animation(true);"  class="btn theme-background-color rounded-3 borders card-1 ml-4 mr-4 custom_button_shadow font-weight-normal">Continue <i class="ml-2 fas fa-arrow-right"></i></a>
 
                </div>
             </div>
