aa92e8896d1df4b273f99aed9f20baba554885e4
anujtaya <tayaanuj@gmail.com>
2021-02-23 11:57:33 +0530

    Fixed text wording on seeker homepage.

--- resources/views/service_seeker/demo/home_1.blade.php
+++ resources/views/service_seeker/demo/home_1.blade.php
@@ -72,7 +72,7 @@
    <div id="map"  style="min-width:100%!important;"></div>
    <div id="over_map_bottom" class="text-center">
       <span id="user_current_saved_location" class="bg-white p-1 fs--2"></span><br>
-      <a class="btn btn-block btn-sm theme-background-color card-1 mt-2" href="{{route('guest_service_seeker_home')}}?showBooking=on&showtutorial=true" onclick="toggle_animation(true);">Service Seeker - I want work done</a>
+      <a class="btn btn-block btn-sm theme-background-color card-1 mt-2" href="{{route('guest_service_seeker_home')}}?showBooking=on&showtutorial=true" onclick="toggle_animation(true);">Switch to Seeker - I want work done</a>
       <a class="btn btn-block btn-sm text-white mt-2 shadow" style="background:#5D29BA!important;color:white!important;" href="{{route('guest_service_provider_home')}}?showtutorial=true" onclick="toggle_animation(true);">Switch to Provider - I want to work</a>
    </div>
 </div>
