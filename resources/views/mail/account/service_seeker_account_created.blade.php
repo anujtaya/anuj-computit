1e3907a48f8285148a180f22fa45a27f5073058e
anujtaya <tayaanuj@gmail.com>
2021-05-12 17:19:48 +0530

    Changed the email template for the welcome email for new users.

--- resources/views/mail/account/service_seeker_account_created.blade.php
+++ resources/views/mail/account/service_seeker_account_created.blade.php
@@ -16,14 +16,8 @@
 <p>
    Welcome to the LocaL2LocaL Community. Thank you for taking the time to update your profile and verify your account. You can now start making service requests.
 </p>
-@component('mail::button', ['url' => $url1])
-Watch intro video
-@endcomponent
-<p>
-   Please take a moment to watch these additional videos to learn more about LocaL2LocaL.
-</p>
 @component('mail::button', ['url' =>$url2])
-Watch additional videos
+Go To Website
 @endcomponent
 We look forward to a future where you can earn extra money in your spare time or find a local to assist immediately when you require any service.
 {{-- Salutation --}}
